f8b29aedbbad28f65216103367f909043dce6898
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 15:27:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  val_1 = 10000
+  val_2 = 10000
 
-  name_hash. do |key, value|
+  name_hash.each do |key, value|
     
   end
   
